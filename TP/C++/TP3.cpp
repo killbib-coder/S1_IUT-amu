7412b4c0263c1ac7b0892f2968c1c5b2abb6ea80
killbib-coder <killbibferrari@gmail.com>
2022-10-12 16:09:06 +0200

    C++

--- TP/C++/TP3.cpp
+++ TP/C++/TP3.cpp
@@ -5,35 +5,35 @@ using namespace std;
 unsigned CompterDoublonWFor(string Chaine){
     if (Chaine.size() < 2) return 0;
     unsigned Compt = 0;
-    for (unsigned i = 0; i < Chaine.size()-2; i++)
+    for (unsigned i = 0; i != Chaine.size()-1; i++)
     {
-        if (Chaine[i] == Chaine[i+1]) Compt=+1;
+        if (Chaine[i] == Chaine[i+1]) Compt+=1;
     }
     return Compt;    
 }
 unsigned CompterDoublonWhile(string Chaine){
     if (Chaine.size() < 2) return 0;
     unsigned Compt = 0, i = 0;
-    while (i < Chaine.size()-2)
+    while (i != Chaine.size()-1)
     {
         if (Chaine[i] == Chaine[i+1]) Compt +=1;
         i+=1;
     }
     return Compt;    
 }
-unsigned CompterDoublonWForInverse(string Chaine){
+unsigned CompterDoublonForInverse(string Chaine){
     if (Chaine.size() < 2) return 0;
     unsigned Compt = 0;
-    for (unsigned i = 1; i < Chaine.size()-1; i++)
+    for (unsigned i = 1; i != Chaine.size(); i++)
     {
-        if (Chaine[i] == Chaine[i-1]) Compt=+1;
+        if (Chaine[i] == Chaine[i-1]) Compt+=1;
     }
     return Compt;    
 }
-unsigned CompterDoublonWhile(string Chaine){
+unsigned CompterDoublonWhileInverse(string Chaine){
     if (Chaine.size() < 2) return 0;
     unsigned Compt = 0, i = 1;
-    while (i < Chaine.size()-1)
+    while (i != Chaine.size())
     {
         if (Chaine[i] == Chaine[i-1]) Compt +=1;
         i+=1;
@@ -41,7 +41,10 @@ unsigned CompterDoublonWhile(string Chaine){
     return Compt;    
 }
 
-int main(){
 
+int main(){
+    string Str = "aa bn cc";
+    unsigned resultTest = CompterDoublonForInverse(Str);
+    cout << resultTest;
     return 0;
 }
