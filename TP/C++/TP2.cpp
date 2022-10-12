1bbeef6c70cf3f6ad0f135576a15cf07ef31ad40
killbib-coder <killbibferrari@gmail.com>
2022-10-12 14:26:21 +0200

    C++

--- TP/C++/TP2.cpp
+++ TP/C++/TP2.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
+#include "EasyAssert.h"
 
-unsigned TrouveCarDansString (char & Caract, string & Chaine,unsigned & Debut){
+unsigned TrouveCarDansString (char Caract, string Chaine,unsigned Debut){
     unsigned i;
     i = Debut;
     while (i != Chaine.size() and Caract != Chaine[i])
@@ -11,10 +12,14 @@ unsigned TrouveCarDansString (char & Caract, string & Chaine,unsigned & Debut){
     return i;
     
 }
+void AssertTrouveCarDansString(char Caract, string Chaine,unsigned Debut){
+    EASY_ASSERT(TrouveCarDansString(Caract, Chaine, Debut));
+}
 
 int main(){
     string str;
     str = "Salut ca va ?";
-    cout << TrouveCarDansString('a', str, 0);
+    cout << TrouveCarDansString('c', str, 0) << endl;
+    AssertTrouveCarDansString('a', str, 0);
     return 0;
 }
