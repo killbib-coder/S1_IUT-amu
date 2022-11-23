59250bed5a3cd5df2e1eb1911624058f2b7ad812
killbib-coder <killbibferrari@gmail.com>
2022-11-23 11:24:01 +0100

    Update

--- Project/R1.01-Project/vote/C++/main.cpp
+++ Project/R1.01-Project/vote/C++/main.cpp
@@ -4,30 +4,39 @@ using namespace std;
 
 int main(){
 
-    string str, temp;
-    unsigned nbelecteur(0);
-    cin >> str;
-    nbelecteur=stoi(str);
-    vector<string> noms(nbelecteur);
-    vector<string> vote(nbelecteur);
-    unsigned cptVote=0;
-    for (size_t i = 0; i < nbelecteur; i++)
-    {
-        cin >> str;
-        temp=str;
-        cin >> str;
-        noms[i]=temp+" "+str+" ";
-        cin >> str;
-        vote[cptVote]=str;
-        cptVote++;
-    }
+    string str,nom;
 
-    //afficher vecteurs
+    unsigned i=1,compteur=0;
+    vector<string> noms;
+    vector<string> vote;
+    unsigned cptVote=0;
+    while (true)
+    {
+        getline(cin,str);
+        if (cin.eof()) break;    
+        
+        if (compteur==0){
+            nom=str;
+            compteur++;
+        }
+        else if (compteur==1){
+            noms.push_back(nom+" "+str);
+            compteur++;
+        }
+        else if (compteur==2){
+            vote.push_back(str);
+            cptVote++;
+            compteur=0;
+        }
+        i++;
+        
+    }
+    cout << endl << endl;
     for (size_t i = 0; i < noms.size(); i++)
     {
-        cout << noms[i];
+        cout << noms[i]<<" / ";
     }
-    cout << endl;
+    cout << endl << endl;
     for (size_t i = 0; i < vote.size(); i++)
     {
         cout << vote[i];
